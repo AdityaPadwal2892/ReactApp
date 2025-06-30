f30da1103c679b00d3e2d5730d86808988696874
Aditya <adityapadwal2892@gmail.com>
2025-06-30 21:03:58 +0530

    Updated contact component and package files

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,28 @@
 // Contact.js
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
 import './Contact.css';
 
 function Contact({ showModal, closeForm }) {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1sshgxd', 'template_e3qjp46', form.current, 'iT3j_yCALx2ru9FzX')
+      .then((result) => {
+          alert("Message sent successfully!");
+          closeForm();
+      }, (error) => {
+          alert("Failed to send message. Try again later.");
+      });
+  };
+
+
+
   if (!showModal) return null;
 
   return (
@@ -10,10 +30,10 @@ function Contact({ showModal, closeForm }) {
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <span className="close-button" onClick={closeForm}>&times;</span>
         <h2>Contact Us</h2>
-        <form className="contact-form">
-          <input type="text" placeholder="Your Name" required />
-          <input type="email" placeholder="Your Email" required />
-          <textarea placeholder="Your Message" required />
+        <form className="contact-form" ref={form} onSubmit={sendEmail}>
+          <input type="text" name="user_name" placeholder="Your Name" required />
+          <input type="email"  name="user_email"  placeholder="Your Email" required />
+          <textarea name="message"  placeholder="Your Message" required />
           <button type="submit">Send Message</button>
         </form>
       </div>
